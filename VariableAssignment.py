26e4a804fc0f6a53d8fd11f2b63e127c144fe1a1
Niwesa Neupane <neupaneniwesa@gmail.com>
2025-01-27 16:17:59 -0600

    Variables Assignment

---
+++ VariableAssignment.py
@@ -0,0 +1,17 @@
+#get name and age of user
+Name = input("Enter your name: ")
+Age = int(input("Enter your age: "))
+
+#True if it is greater then 12 false if it is not
+if Age>= 12:
+   print(True)
+else:
+   print(False)
+
+#Display users name and age
+MyList = [Name,Age]
+print(MyList)   
+
+#Add 3.1 to the users age
+print(Age+3.1)
+
